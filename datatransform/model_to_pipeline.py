6230c83cb9a5d624a725b18f5be1ead7317f29a0
Deepthi Chand <eopoxf@gmail.com>
2021-10-09 04:56:55 +0530

    fix module importing

--- datatransform/model_to_pipeline.py
+++ datatransform/model_to_pipeline.py
@@ -2,14 +2,17 @@ from .models import Task, Pipeline
 from concurrent.futures import ThreadPoolExecutor
 from pipeline import pipeline
 
-module = "tasks.example_task.ExampleTask"
-task = __import__(module)
+# module = "tasks.example_task.ExampleTask"
+# task = __import__(module)
+
+mod = __import__('tasks.example_task', fromlist=['ExampleTask'])
+klass = getattr(mod, 'ExampleTask')
 
 
-async def model_to_pipeline(p: Pipeline):
+def model_to_pipeline(p: Pipeline):
     try:
         new_pipeline = pipeline.Pipeline()
-        new_pipeline.add(task.ExampleTask())
+        new_pipeline.add(klass())
         with ThreadPoolExecutor(max_workers=1) as executor:
             future = executor.submit(new_pipeline.execute)
             print(future.result())
