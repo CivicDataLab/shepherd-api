584ef16b4a26aa5bfab0a7691a67492ee1562472
Abhinav <a.kumar2102@gmail.com>
2022-12-07 20:22:14 +0530

    fix json keep column  parent deletion

--- pipeline/api_resource_query_bg.py
+++ pipeline/api_resource_query_bg.py
@@ -26,11 +26,26 @@ graph_ql_url = os.environ.get(
 
 
 def json_keep_column(data, cols):
+    child_keys_list = []
     try:
+        
+        def get_child_keys(d):
+            if isinstance(d,  dict):
+                for key in list(d.keys()):
+                    child_keys_list.append(key)
+                    if isinstance(d[key], dict):
+                        get_child_keys(d[key])  
+            if isinstance(d,  list):
+                for each in d:
+                    if isinstance(each,  dict):
+                        get_child_keys(each)                          
 
         def remove_a_key(d, remove_key):
             for key in list(d.keys()):
-                if key not in remove_key:
+                child_keys_list = []
+                get_child_keys(d[key])
+                print ('--------------', child_keys_list)
+                if key not in remove_key and not any([ item in remove_key for item in child_keys_list]):
                     del d[key]
                 else:
                     keep_col(d[key], remove_key)
