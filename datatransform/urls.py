230ed1d4151a32da5e89b0ddee79276173484e8d
Ubuntu <ubuntu@ip-172-31-21-131.ap-south-1.compute.internal>
2022-12-11 21:13:09 +0000

    changes for json parse and api_res transform

--- datatransform/urls.py
+++ datatransform/urls.py
@@ -6,11 +6,12 @@ from django.views.decorators.csrf import csrf_exempt
 urlpatterns = [
     path('trans_list', views.transformer_list, name='transformer_list'),
     path('pipe_create', csrf_exempt(views.pipe_create), name='pipe_create'),
-path('pipe_update', csrf_exempt(views.pipe_update), name='pipe_update'),
+    path('pipe_update', csrf_exempt(views.pipe_update), name='pipe_update'),
     path('pipe_list', csrf_exempt(views.pipe_list), name='pipe_list'),
     path('res_transform', csrf_exempt(views.res_transform), name='res_transform'),
     path('pipeline_filter', csrf_exempt(views.pipeline_filter), name='pipeline_filter'),
     path('custom_data_viewer', csrf_exempt(views.custom_data_viewer), name='custom_data_viewer'),
     path('api_source_query', csrf_exempt(views.api_source_query), name='api_source_query'),
-    path('api_res_transform', csrf_exempt(views.api_res_transform), name='api_res_transform')
+    path('api_res_transform', csrf_exempt(views.api_res_transform), name='api_res_transform'),
+    path('api_transformer_list', csrf_exempt(views.api_transformer_list), name='api_transformer_list')
 ]
