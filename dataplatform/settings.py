3c975891c41860ab844b607e093a636dfe3c3315
Abhinav <a.kumar2102@gmail.com>
2022-12-16 11:22:21 +0530

    docker changes ad postgres added

--- dataplatform/settings.py
+++ dataplatform/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -93,9 +94,14 @@ WSGI_APPLICATION = 'dataplatform.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": os.environ.get('DB_ENGINE', "django.db.backends.sqlite3"),
+        "NAME": os.environ.get('DB_NAME', BASE_DIR / "db.sqlite3"),
+        "USER": os.environ.get('DB_USER', "DB_USER"),
+        "PASSWORD": os.environ.get('DB_PASSWORD', 'DB_PASSWORD'),
+        "HOST": os.environ.get('DB_HOST', 'DB_HOST'),
+        "PORT":os.environ.get('DB_PORT', 'DB_PORT'), 
+        
     }
 }
 
