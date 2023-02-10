5f1409231528c9b71a762a4034c0fa76e9c0266d
Manjunath <hmanjub@gmail.com>
2023-02-10 16:55:12 +0530

    committing for stash pop

--- rabbitmq_tryout/send.py
+++ rabbitmq_tryout/send.py
@@ -1,114 +1,78 @@
-# import json
-# import random
-#
-# import pandas as pd
-# import requests
-# #
-# #
-# # def skip_col(d, remove_key):
-# #     if isinstance(d, dict):
-# #         remove_a_key(d, remove_key)
-# #     if isinstance(d, list):
-# #         for each in d:
-# #             if isinstance(each, dict):
-# #                 remove_a_key(each, remove_key)
-# #     return d
-# #
-# # f = open('data.json', 'rb')
-# # data = json.load(f)
-# # trans_data = skip_col(data, ['updated_date','title'])
-# # print(trans_data)
-#
-# sep = "|"
-# op_col_name = "result"
-#
-#
-# def merge_cols(d, cols_list, result):
-#     global final_val
-#     for key in list(d.keys()):
-#         if key in cols_list:
-#             result.append(d[key])
-#         else:
-#             merge_col(d[key], cols_list, result)
-#
-#
-# def merge_col(d, cols_list, result):
-#     if isinstance(d, dict):
-#         merge_cols(d, cols_list, result)
-#     if isinstance(d, list):
-#         for each in d:
-#             if isinstance(each, dict):
-#                 val_list = []
-#                 for key in list(each.keys()):
-#                     if key in cols_list:
-#                         print("got a key")
-#                         val_list.append(each[key])
-#                         print(val_list, "value list...")
-#                         if len(val_list) == 2:
-#                             result_str = str(sep).join(val_list)
-#                             each[op_col_name] = result_str
-#                 merge_cols(each, cols_list, result)
-#     print(result)
-#
-#
-# f = open('data.json', 'rb')
-# data = json.load(f)
-# result = []
-# merge_col(data, ["a", "f"], result)
-# merged = str(sep).join(result)
-# print(merged, "merged  ")
-# if isinstance(data, dict):
-#     data[op_col_name] = merged
-# print(data, "data..")
-import csv
 import json
+import random
 
-import pandas as pd
-import pdfkit
+option = "replace_all"
+special_char = "*"
+n = "2"
 
-#
-# def remove_a_key(d, remove_key):
-#     for key in list(d.keys()):
-#         if key in remove_key:
-#             del d[key]
-#         else:
-#             skip_col(d[key], remove_key)
-#
-#
-# def skip_col(d, remove_key):
-#     if isinstance(d, dict):
-#         remove_a_key(d, remove_key)
-#     if isinstance(d, list):
-#         for each in d:
-#             if isinstance(each, dict):
-#                 remove_a_key(each, remove_key)
-#     return d
-#
-# data = pd.read_json('data.json')
-# pdata = skip_col(data, "gender")
-# print(pdata)
-import pandas as pd
-from pandas.io.json import build_table_schema
+def anonymize_a_key(d, change_key):
+    for key in list(d.keys()):
+        if key == change_key:
+            print("got change key..")
+            val = d[key]
+            print("value type is...", type(val))
+            if isinstance(val, int) or isinstance(val, float):
+                print("it is INT")
+                val = str(val)
+            if isinstance(val, str):
+                if option == "replace_all":
+                    if special_char == "random":
+                        replace_val = "".join(random.choices("!@#$%^&*()<>?{}[]~`", k=len(val)))
+                        d[key] = replace_val
+                    else:
+                        replace_val = special_char * len(val)
+                        d[key] = replace_val
+                elif option == "replace_nth":
+                    # n = context.get('n')
+                    # n = int(n) - 1
+                    # if special_char == "random":
+                    #     replacement = "".join(random.choices("!@#$%^&*()<>?{}[]~`", k=1))
+                    #     replace_val = val[0:int(n)] + replacement + val[int(n) + 1:]
+                    #     d[key] = replace_val
+                    # else:
+                    #     replace_val = val[0:int(n)] + special_char + val[int(n) + 1:]
+                    #     d[key] = replace_val
+                    n = int(n)  # - 1
+                    if special_char == "random":
+                        replacement = "".join(random.choices("!@#$%^&*()<>?{}[]~`", k=1))
 
-index = ["name"]
-columns = ["sub"]
-values = ["class"]
-data = pd.read_csv("test1.csv")
-agged = pd.pivot_table(data, index=index, columns=columns, values=values, aggfunc='count')
-# agged.columns = agged.columns.to_flat_index()
-none_list = [None]
-for i in columns:
-    none_list.append(i)
-print(type(agged.columns))
-agged = agged.rename_axis(none_list, axis=1)
-agged = agged.reset_index()
-print(agged, "after....")
-file_path = "test.csv"
-print(type(agged.columns))
-agged.to_csv("test.csv", index=True)
-inferred_schema = build_table_schema(agged)
-fields = inferred_schema['fields']
-print(fields)
+                        for i in range((n - 1), len(val) + (n - 1), n):
+                            val = val[: i] + replacement + val[i + 1:]
+                            # for i in range(0, len(val), int(n)):
+                        #     val = val[ : i] + replacement + val[i + 1: ] 
+                        # replace_val = val[0:int(n)] + replacement + val[int(n) + 1:]
+                        d[key] = val
+                    else:
+                        for i in range((n - 1), len(val) + (n - 1), n):
+                            val = val[: i] + special_char + val[i + 1:]
+                            # for i in range(0, len(val), int(n)):
+                        #     val = val[ : i] + special_char + val[i + 1: ] 
+                        d[key] = val
+                elif option == "retain_first_n":
+                    if special_char == "random":
+                        replacement = "".join(random.choices("!@#$%^&*()<>?{}[]~`", k=(len(val) - int(n))))
+                        replace_val = val[:int(n)] + replacement
+                        d[key] = replace_val
+                    else:
+                        replace_val = val[:int(n)] + (special_char * (len(val) - int(n)))
+                        d[key] = replace_val
+        else:
+            anonymize_col(d[key], change_key)
 
 
+def anonymize_col(d, change_key):
+    if isinstance(d, dict):
+        anonymize_a_key(d, change_key)
+    if isinstance(d, list):
+        for each in d:
+            if isinstance(each, dict):
+                anonymize_a_key(each, change_key)
 
+    return d
+
+data_file = open("data.json")
+data = json.load(data_file)
+final = anonymize_col(data, "Temperature_Maximum")
+json_obj = json.dumps(final)
+with open("transformed.json", "w") as f:
+    f.write(json_obj)
